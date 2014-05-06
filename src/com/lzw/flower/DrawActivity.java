b594362aa4a6f1b9eae3f80eb2d39b2d5eb1abbc
lzwjava <lzwjava@gmail.com>
2014-05-06 23:11:40 +0800

    fix upload failed bug

--- src/com/lzw/flower/DrawActivity.java
+++ src/com/lzw/flower/DrawActivity.java
@@ -12,6 +12,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -223,9 +224,10 @@ public class DrawActivity extends Activity implements View.OnClickListener {
       @Override
       protected Void doInBackground(Void... params) {
         try {
-          AVFile origin = AVFile.withAbsoluteLocalPath("" + serverId, originPath);
-          AVFile hand = AVFile.withAbsoluteLocalPath("" + serverId, handPath);
+          AVFile origin = AVFile.withAbsoluteLocalPath("origin.png", originPath);
           origin.save();
+          //Thread.sleep(1000);
+          AVFile hand = AVFile.withAbsoluteLocalPath("hand.png", handPath);
           hand.save();
           String originUrl = origin.getUrl();
           String handUrl = hand.getUrl();
