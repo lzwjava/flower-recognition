645031cdbd4d02cb0bca0f35de8dc4b80ad0be47
lzwjava <lzwjava@gmail.com>
2014-07-19 21:29:06 +0800

    update

--- src/com/lzw/flower/base/App.java
+++ src/com/lzw/flower/base/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
   public static final int DRAW_FORE = 1;
   public static final int DRAW_RECT = 2;
   public static final int ALL_INFO = 3;
-  public static boolean debug = true;
+  public static boolean debug = false;
   public static String json = "{     \"valid\" : true, \t\"images\": [ \t{ \"typeName\": \"牡丹\", " +
       "\"typeDesc\" : \"洛阳市花\", \"ImageUrl\" : \"http://paas-files.qiniudn.com/Ex0tsNkAPXoopBf77vwwuJY4vtnX7SCSOtPuaY1D.jpg\", \"InfoUrl\" : \"http://www.baidu.com/洛阳牡丹.html\"}, \t{ \"typeName\": \"蝴蝶兰\", \"typeDesc\" : \"特别好看的花\", \"ImageUrl\" : \"http://paas-files.qiniudn.com/WFlEbl7G0R7PWcjyA34jCO0qmphkaUTcwDiLG0vh.jpg\", \"InfoUrl\" : \"http://www.baidu.com/蝴蝶兰.html\"}, \t{ \"typeName\": \"月季\", \"typeDesc\" : \"洛阳市花\", \"ImageUrl\" : \"http://paas-files.qiniudn.com/NqDVEacbFKgbtqoU1clKEeK5ih48WJF3AEWCVhh9.jpg\", \"InfoUrl\" : \"http://www.baidu.com\"}, \t{ \"typeName\": \"普通花\", \"typeDesc\" : \"洛阳市花\", \"ImageUrl\" : \"http://paas-files.qiniudn.com/JaBVo2WkrFsMnqEtK7QxqLIC1wCAMNEEUcxK2826.jpg\", \"InfoUrl\" : \"http://www.baidu.com\"}, \t{ \"typeName\": \"花3\", \"typeDesc\" : \"洛阳市花\", \"ImageUrl\" : \"http://paas-files.qiniudn.com/V3iz5xDeLx9HdCigbFOKAACuieI3B5ozRjSqN3Uw.jpg\", \"InfoUrl\" : \"http://www.baidu.com\"} \t] }";
 
